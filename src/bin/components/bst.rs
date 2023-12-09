0422ee9863996a14baafe771308079c35187ad3d
Naginipython <naginipython332@gmail.com>
2023-12-09 09:42:08 -0500

    Tried to make code prettier

--- src/bin/components/bst.rs
+++ src/bin/components/bst.rs
@@ -22,58 +22,54 @@ impl<T: Ord> BST<T> {
             branch: &mut Option<Box<Node<T>>>, 
             node: Node<T>
         ) {
-            if let Some(branch_node) = branch {
-                match node.get_data().cmp(branch_node.get_data()) {
-                    Ordering::Greater => helper(&mut branch_node.right, node),
-                    Ordering::Less => helper(&mut branch_node.left, node),
-                    _ => {}
-                }
-            } else {
-                *branch = Some(Box::new(node));
-
+            match branch {
+                Some(branch_node) =>
+                    match node.get_data().cmp(branch_node.get_data()) {
+                        Ordering::Greater => helper(&mut branch_node.right, node),
+                        Ordering::Less => helper(&mut branch_node.left, node),
+                        _ => {}
+                    },
+                None => *branch = Some(Box::new(node))
             }
         }
         
-        if let Some(root_node) = self.root.as_mut() {
-            match node.get_data().cmp(root_node.get_data()) {
-                Ordering::Greater => helper(&mut root_node.right, node),
-                Ordering::Less => helper(&mut root_node.left, node),
-                _ => {}
-            }
-        } else {
-            self.root = Some(node);
+        match self.root.as_mut() {
+            Some(root_node) => // Compares node to to root's data
+                match node.get_data().cmp(root_node.get_data()) {
+                    Ordering::Greater => helper(&mut root_node.right, node),
+                    Ordering::Less => helper(&mut root_node.left, node),
+                    _ => {}
+                },
+            None => self.root = Some(node)
         }
     }
 
     pub fn get_root(&self)-> Result<&T, Box<dyn Error>> {
-        if let Some(root) = &self.root {
-            Ok(root.get_data())
-        } else {
-            Err("Could not get root")?
+        match &self.root {
+            Some(root) => Ok(root.get_data()),
+            None => Err("Could not get root")?
         }
     }
 
     pub fn get_left(&self) -> Result<&T, Box<dyn Error>> {
-        if let Some(root) = &self.root {
-            if let Some(branch) = &root.left {
-                Ok(branch.get_data())
-            } else {
-                Err("Could not get left")?
-            }
-        } else {
-            Err("Could not get root")?
+        match &self.root {
+            Some(root) => 
+                match &root.left {
+                    Some(branch) => Ok(branch.get_data()),
+                    None => Err("Could not get left")?
+                },
+            None => Err("Could not get root")?
         }
     }
 
     pub fn get_right(&self) -> Result<&T, Box<dyn Error>> {
-        if let Some(root) = &self.root {
-            if let Some(branch) = &root.right {
-                Ok(branch.get_data())
-            } else {
-                Err("Could not get left")?
-            }
-        } else {
-            Err("Could not get root")?
+        match &self.root {
+            Some(root) => 
+                match &root.right {
+                    Some(branch) => Ok(branch.get_data()),
+                    None => Err("Could not get right")?
+                },
+            None => Err("Could not get root")?
         }
     }
 }
